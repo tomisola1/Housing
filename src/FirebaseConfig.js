8c7f43195e843aefe8daec75c154b7f63763d7b6
tomisola1 <tomisoladebby@gmail.com>
2023-02-06 22:11:19 +0100

    project setup

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZnNoer-MhrS8F3U5FbedtsO2Gope7xfc",
+  authDomain: "housing-1e300.firebaseapp.com",
+  projectId: "housing-1e300",
+  storageBucket: "housing-1e300.appspot.com",
+  messagingSenderId: "72049431078",
+  appId: "1:72049431078:web:4a54cf4fea02acca1d0ce7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
