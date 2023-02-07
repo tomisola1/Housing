abbe2350fbf4c6f4635cd35f0a918799543609b7
tomisola1 <tomisoladebby@gmail.com>
2023-02-07 23:38:48 +0100

    Auth is done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 // import Category from "./pages/Category";
@@ -6,7 +9,11 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
-import Navbar from "./comonents/Navbar";
+import Navbar from "./components/Navbar";
+// import CreateListing from "./pages/CreateListing";
+// import EditListing from "./pages/EditListing";
+// import Listing from "./pages/Listing";
+// import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -16,9 +23,9 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
           {/* <Route path="/category/:categoryName" element={<Category />} /> */}
-          {/* <Route path='/profile' element={<PrivateRoute />}> */}
-          <Route path="/profile" element={<Profile />} />
-          {/* </Route> */}
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
@@ -33,7 +40,7 @@ function App() {
         <Navbar />
       </Router>
 
-      {/* // <ToastContainer /> */}
+      <ToastContainer />
     </>
   );
 }
